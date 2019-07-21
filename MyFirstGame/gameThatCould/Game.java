07358fac1c28df94e3f79ca6a8cb48dfbb59ea0d
Jesse_gotemmm D <jesse.o.dickson@leidos.com>
2019-07-20 19:40:41 -0500

    game class modified

--- MyFirstGame/gameThatCould/Game.java
+++ MyFirstGame/gameThatCould/Game.java
@@ -10,6 +10,10 @@ public class Game extends Canvas implements Runnable {
 	//setting up the size of the game window  
 	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
 	
+	private Thread thread;
+	private boolean running = false;
+	
+	
 	public Game()
 	{
 		new Window(WIDTH, HEIGHT, "Jesse's First Game Chea Yeah", this);
@@ -17,19 +21,54 @@ public class Game extends Canvas implements Runnable {
 
 	public synchronized void start()
 	{
-		
+		thread = new Thread(this);
+		thread.start();
+		running = true;
 	}
+	
+	public synchronized void stop()
+	{
+		try {
+			thread.join();
+			running = false;
+		}catch(Exception j) {
+			j.printStackTrace();
+		}
+	}
+	
 	
 	public void run()
 	{
-		
+		long lastTime = System.nanoTime();
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running)
+		{
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1)
+			{
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000)
+			{
+				
+			}
+		}
 	}
 	
 	// a main?
 	public static void main(String[] args)
 	{
-
-		
+		new Game();
 	}
 
 }
